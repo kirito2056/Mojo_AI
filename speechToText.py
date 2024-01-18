020764d3d8049d3d562e154bb0d8b7d662552c76
하진 권씨 <hajin_2056@naver.com>
2024-01-18 17:22:40 +0900

    timeout 추가

--- speechToText.py
+++ speechToText.py
@@ -1,15 +1,20 @@
 import speech_recognition as sr
+
 recognizer = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print("말하세요...")
+    print("자 말해보시오")
     recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.listen(source)
+
+    try:
+        audio = recognizer.listen(source, timeout=5)
+    except sr.WaitTimeoutError:
+        print("마이크가 당신의 목소리를 못듣고 있는거 같소")
 
 try:
     text = recognizer.recognize_google(audio, language="ko-KR")
     print("인식된 텍스트: " + text)
 except sr.UnknownValueError:
-    print("음성을 인식할 수 없습니다.")
+    print("뭐래는겨 똑바로 말해봐")
 except sr.RequestError as e:
-    print(f"Google Web Speech API 오류: {e}")
+    print(f"이건 구글 오류인디 : {e}")
