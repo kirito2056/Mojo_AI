11d2f137524c92788def9dc566bffc10041fe6a9
하진 권씨 <hajin_2056@naver.com>
2024-01-31 22:36:08 +0900

    일단 약 인공지능으로 만들어야겠다

--- YUNA.py
+++ YUNA.py
@@ -8,7 +8,18 @@ def listen(recognizer, audio):
     try:
         text = recognizer.recognize_google(audio, language='ko-KR')
         print('[사용자] ' + text)
+        answer(text)
     except sr.UnknownValueError:
         print('인식 실패')
     except sr.RequestError as e:
         print('요청 실패 : {0}'.format(e))
+
+#대답 생성
+def answer(input_text):
+    answer_text = ''
+    if '안녕' in input_text:
+        answer_text = '안녕하세요? 반갑습니다.'
+    elif '날씨' in input_text:
+        answer_text = '아직 개발해야 하는 기능입니다' 
+    else:
+        answer_text = '다시 한번 말씀해주세요'
